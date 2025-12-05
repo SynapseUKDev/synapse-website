1227f90e6108062d723058091a89be0ce8d5faaa
ayazb7 <ayazbaig@hotmail.co.uk>
2025-12-05 09:58:17 +0000

    Maintenance mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Maintenance from './Maintenance.jsx'
+
+const isMaintenance =
+  typeof import.meta !== 'undefined' &&
+  import.meta.env &&
+  import.meta.env.VITE_MAINTENANCE_MODE === 'true'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isMaintenance ? <Maintenance /> : <App />}
   </StrictMode>,
 )
